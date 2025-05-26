24cac3a747fee1a565ed389be7e448bcee504d99
Jonathan Meléndez Macedonio <jmelendez@skleasing.mx>
2025-05-26 10:01:11 -0600

    Segundo commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/skleasing-react",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 export default nextConfig;
