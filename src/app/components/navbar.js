7129e61ba822c17c65d9991bc01b25040466d712
Jonathan Meléndez Macedonio <jmelendez@skleasing.mx>
2025-05-26 09:40:38 -0600

    first commit

---
+++ src/app/components/navbar.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function NavBar({}){
+
+    return (
+        <nav className="flex flex-col items-end fixed top-0 inset-x-0 z-50 flex-none p-2 h-14 md:my-2 md:rounded-full border-2 border-solid border-gray-400 md:border-0.5 backdrop-blur-lg md:top-6 md:left-1/2 md:right-auto md:-translate-x-1/2 text-primary shadow-xl bg-white">
+            <div className="flex gap-2 justify-between items-center w-full md:gap-px">
+                <Link href="#"><Image priority={true} sizes="(max-width: 768px) 500px, 500px" className="px-3 w-28 " src="/images/SK-Leasing-logo.png" alt="Logo de SK Leasing" width={500} height={119}/></Link>
+                <Link href="#" target="_self" className="hidden select-none px-3 md:flex items-center font-medium transition-all py-1.5 rounded-full border flex-none hover:bg-black/5 border-transparent text-tertiary">Nosotros</Link>
+                <Link href="#" target="_self" className="hidden select-none px-3 md:flex items-center font-medium transition-all py-1.5 rounded-full border flex-none hover:bg-black/5 border-transparent text-tertiary">Beneficios</Link>
+                <Link href="#" target="_self" className="hidden select-none px-3 md:flex items-center font-medium transition-all py-1.5 rounded-full border flex-none hover:bg-black/5 border-transparent text-tertiary">Contáctanos</Link>
+                <button data-tally-open="mOyxaR" data-tally-layout="modal" data-tally-width="500" data-tally-auto-close="0" className="select-none flex items-center font-medium transition-all py-1.5 rounded-full border flex-none px-3 bg-accent hover:bg-accent-hover border-transparent text-white ml-1.5 plausible-event-name=windows-waitlist">
+                    <Link href="#">Cotizar</Link>
+                </button>
+            </div>
+        </nav>
+    );
+}
---
+++ src/app/layout.js
@@ -0,0 +1,66 @@
+import "@/app/ui/globals.css";
+import Script from "next/script";
+
+export const metadata = {
+    title: {
+        template: "SK Leasing | %s",
+        default: "SK Leasing | Especialistas en arrendamiento puro, enfocados al servicio al cliente",
+    },
+    description: "SK Leasing (Promotora SKU, S.A.P.I. de C.V.) es una empresa 100% mexicana especializada en Arrendamiento Puro, enfocada en el Servicio al Cliente, y en la Velocidad de Respuesta.",
+    author: "SK Leasing",
+    generator: "Next.js",
+    referrer: 'origin-when-cross-origin',
+    creator: 'Jonathan Meléndez Macedonio',
+    publisher: 'Jonathan Meléndez Macedonio',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    openGraph: {
+        title: "SK Leasing | Especialistas en arrendamiento puro, enfocados al servicio al cliente",
+        description: "SK Leasing (Promotora SKU, S.A.P.I. de C.V.) es una empresa 100% mexicana especializada en Arrendamiento Puro, enfocada en el Servicio al Cliente, y en la Velocidad de Respuesta.",
+        url: "https://skleasing.mx",
+        siteName: "SK Leasing",
+        locale: "es_MX",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        googleBot: {
+            index: true,
+            follow: true,
+            noimageindex: false,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "SK Leasing | Especialistas en arrendamiento puro, enfocados al servicio al cliente",
+        description: "SK Leasing (Promotora SKU, S.A.P.I. de C.V.) es una empresa 100% mexicana especializada en Arrendamiento Puro, enfocada en el Servicio al Cliente, y en la Velocidad de Respuesta.",
+    },
+    verification: {
+        google: "bUIJYev_Rajl5CGrCFb_S0jM2-O5FNXYUiaTZ_0kZOE",
+    }
+}
+
+export const viewport = {
+    themeColor: '#075892',
+    maximumScale: 1,
+    userScalable: false,
+    colorScheme: "light dark",
+}
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en" className="h-full">
+            <body className="h-full">
+                <Script src="https://tally.so/widgets/embed.js" strategy="beforeInteractive" async={true}/>
+                {children}
+            </body>
+        </html>
+    );
+}
