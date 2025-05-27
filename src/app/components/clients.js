e3e20a6d929676a67af14da1e22bb705eb29a167
Jonathan Meléndez Macedonio <jmelendez@skleasing.mx>
2025-05-27 11:09:24 -0600

    Actualización en imagenes

--- src/app/components/clients.js
+++ src/app/components/clients.js
@@ -6,54 +6,54 @@ export default function Clients(){
             <h3 className="mb-8 text-xl font-medium text-secondary text-center">Algunas empresas que confían en nosotros</h3>
             <div className="flex overflow-hidden relative">
                 <div className="flex flex-none gap-4 animate-clients">
-                    <Image alt="Logo de Actica" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/actica_logo.png"/>
-                    <Image alt="Logo de Aserta" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/aserta_logo.png"/>
-                    <Image alt="Logo de Refaccionaria California" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/autopartes.png"/>
-                    <Image alt="Logo de Bancoppel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/bancoppel_logo.png"/>
-                    <Image alt="Logo de Barcel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/barcel_logo.png"/>
-                    <Image alt="Logo de BMS" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/bms_logo.png"/>
-                    <Image alt="Logo de Conpryssa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/conpryssa_logo.png"/>
-                    <Image alt="Logo de Cotemar" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/cotemar_logo.jpeg"/>
-                    <Image alt="Logo de Crepes & Waffles" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/crepes.png"/>
-                    <Image alt="Logo de Fincomun" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/fincomun_logo.png"/>
+                    <Image alt="Logo de Actica" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/actica_logo.png"/>
+                    <Image alt="Logo de Aserta" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/aserta_logo.png"/>
+                    <Image alt="Logo de Refaccionaria California" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/autopartes.png"/>
+                    <Image alt="Logo de Bancoppel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/bancoppel_logo.png"/>
+                    <Image alt="Logo de Barcel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/barcel_logo.png"/>
+                    <Image alt="Logo de BMS" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/bms_logo.png"/>
+                    <Image alt="Logo de Conpryssa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/conpryssa_logo.png"/>
+                    <Image alt="Logo de Cotemar" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/cotemar_logo.jpeg"/>
+                    <Image alt="Logo de Crepes & Waffles" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/crepes.png"/>
+                    <Image alt="Logo de Fincomun" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/fincomun_logo.png"/>
                 </div>
                 <div className="flex flex-none gap-4 animate-clients">
-                    <Image alt="Logo de Actica" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/actica_logo.png"/>
-                    <Image alt="Logo de Aserta" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/aserta_logo.png"/>
-                    <Image alt="Logo de Refaccionaria California" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/autopartes.png"/>
-                    <Image alt="Logo de Bancoppel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/bancoppel_logo.png"/>
-                    <Image alt="Logo de Barcel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/barcel_logo.png"/>
-                    <Image alt="Logo de BMS" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/bms_logo.png"/>
-                    <Image alt="Logo de Conpryssa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/conpryssa_logo.png"/>
-                    <Image alt="Logo de Cotemar" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/cotemar_logo.jpeg"/>
-                    <Image alt="Logo de Crepes & Waffles" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/crepes.png"/>
-                    <Image alt="Logo de Fincomun" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/fincomun_logo.png"/>
+                    <Image alt="Logo de Actica" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/actica_logo.png"/>
+                    <Image alt="Logo de Aserta" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/aserta_logo.png"/>
+                    <Image alt="Logo de Refaccionaria California" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/autopartes.png"/>
+                    <Image alt="Logo de Bancoppel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/bancoppel_logo.png"/>
+                    <Image alt="Logo de Barcel" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/barcel_logo.png"/>
+                    <Image alt="Logo de BMS" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/bms_logo.png"/>
+                    <Image alt="Logo de Conpryssa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/conpryssa_logo.png"/>
+                    <Image alt="Logo de Cotemar" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/cotemar_logo.jpeg"/>
+                    <Image alt="Logo de Crepes & Waffles" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/crepes.png"/>
+                    <Image alt="Logo de Fincomun" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/fincomun_logo.png"/>
                 </div>
                 <div className="absolute left-0 w-1/3 h-full bg-gradient-to-l from-transparent to-white"></div>
                 <div className="absolute right-0 w-1/3 h-full bg-gradient-to-r from-transparent to-white"></div>
             </div>
             <div className="flex overflow-hidden relative">
                 <div className="flex flex-none gap-4 animate-clients-reverse">
-                    <Image alt="Logo de Gulf" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/gulf_logo.png"/>
-                    <Image alt="Logo de Hycsa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/hycsa_logo.png"/>
-                    <Image alt="Logo de Rosedal" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/logo_rosedal.png"/>
-                    <Image alt="Logo de Lojack" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/lojack_logo.png"/>
-                    <Image alt="Logo de Marriot" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/marriot.png"/>
-                    <Image alt="Logo de Momentive" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/momentive_logo.png"/>
-                    <Image alt="Logo de Parque Reforma" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/parque.png"/>
-                    <Image alt="Logo de Sonora Grill" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/sonoragrill_logo.png"/>
-                    <Image alt="Logo de Sushitto" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/sushitto.png"/>
+                    <Image alt="Logo de Gulf" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/gulf_logo.png"/>
+                    <Image alt="Logo de Hycsa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/hycsa_logo.png"/>
+                    <Image alt="Logo de Rosedal" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/logo_rosedal.png"/>
+                    <Image alt="Logo de Lojack" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/lojack_logo.png"/>
+                    <Image alt="Logo de Marriot" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/marriot.png"/>
+                    <Image alt="Logo de Momentive" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/momentive_logo.png"/>
+                    <Image alt="Logo de Parque Reforma" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/parque.png"/>
+                    <Image alt="Logo de Sonora Grill" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/sonoragrill_logo.png"/>
+                    <Image alt="Logo de Sushitto" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/sushitto.png"/>
                 </div>
                 <div className="flex flex-none gap-4 animate-clients-reverse">
-                    <Image alt="Logo de Gulf" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/gulf_logo.png"/>
-                    <Image alt="Logo de Hycsa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/hycsa_logo.png"/>
-                    <Image alt="Logo de Rosedal" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/logo_rosedal.png"/>
-                    <Image alt="Logo de Lojack" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/lojack_logo.png"/>
-                    <Image alt="Logo de Marriot" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/marriot.png"/>
-                    <Image alt="Logo de Momentive" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/momentive_logo.png"/>
-                    <Image alt="Logo de Parque Reforma" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/parque.png"/>
-                    <Image alt="Logo de Sonora Grill" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/sonoragrill_logo.png"/>
-                    <Image alt="Logo de Sushitto" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="/images/clients/sushitto.png"/>
+                    <Image alt="Logo de Gulf" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/gulf_logo.png"/>
+                    <Image alt="Logo de Hycsa" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/hycsa_logo.png"/>
+                    <Image alt="Logo de Rosedal" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/logo_rosedal.png"/>
+                    <Image alt="Logo de Lojack" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/lojack_logo.png"/>
+                    <Image alt="Logo de Marriot" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/marriot.png"/>
+                    <Image alt="Logo de Momentive" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/momentive_logo.png"/>
+                    <Image alt="Logo de Parque Reforma" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/parque.png"/>
+                    <Image alt="Logo de Sonora Grill" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/sonoragrill_logo.png"/>
+                    <Image alt="Logo de Sushitto" loading="lazy" width={240} height={120} decoding="async" className="w-40 opacity-80 md:w-48" src="./images/clients/sushitto.png"/>
                 </div>
                 <div className="absolute left-0 w-1/3 h-full bg-gradient-to-l from-transparent to-white"></div>
                 <div className="absolute right-0 w-1/3 h-full bg-gradient-to-r from-transparent to-white"></div>
